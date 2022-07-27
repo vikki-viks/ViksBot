1a9a28f8ae887c42816f1197b1738ab5687b3e76
vikki-viks <vicka.kudryavtseva@yandex.ru>
2022-07-27 12:40:52 +0300

    create options

--- index.js
+++ index.js
@@ -4,32 +4,12 @@ const Telegrambot = require("node-telegram-bot-api");
 const token = process.env.TOKEN;
 const chats = {};
 const bot = new Telegrambot(token, { polling: true });
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: "1", callback_data: "1" },
-        { text: "2", callback_data: "2" },
-        { text: "3", callback_data: "3" },
-      ],
-      [
-        { text: "4", callback_data: "4" },
-        { text: "5", callback_data: "5" },
-        { text: "6", callback_data: "6" },
-      ],
-      [
-        { text: "7", callback_data: "7" },
-        { text: "8", callback_data: "8" },
-        { text: "9", callback_data: "9" },
-      ],
-      [{ text: "0", callback_data: "0" }],
-    ],
-  }),
-};
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "Играть еще раз", callback_data: "/again" }]],
-  }),
+const { gameOptions, againOptions } = require("./options");
+
+const startGame = async (chatId) => {
+  const random = Math.floor(Math.random() * 10);
+  chats[chatId] = random;
+  await bot.sendMessage(chatId, "Отгадай цифру от 0 до 9", gameOptions);
 };
 const start = () => {
   bot.setMyCommands([
@@ -42,23 +22,28 @@ const start = () => {
     const chatId = msg.chat.id;
 
     if (text === "/start") {
-      bot.sendMessage(chatId, "Welcome");
+      return bot.sendMessage(chatId, "Добро пожаловать!");
     }
     if (text === "/game") {
-      const random = Math.floor(Math.random() * 10);
-      chats[chatId] = random;
-      return bot.sendMessage(chatId, "Отгадай цифру от 0 до 9", gameOptions);
+      return startGame(chatId);
     }
-    return bot.sendMessage(chatId, "Try again");
+    return bot.sendMessage(chatId, "Я Вас не понимаю");
   });
 
   bot.on("callback_query", async (msg) => {
     const data = msg.data;
     const chatId = msg.message.chat.id;
+    if (data === "/again") {
+      return startGame(chatId);
+    }
     if (data === chats[chatId]) {
-      return bot.sendMessage(chatId, "Congradulations!");
+      return bot.sendMessage(chatId, "Поздравляю!", againOptions);
     } else {
-      return bot.sendMessage(chatId, `Бот загад цифру ${chats[chatId]}`);
+      return bot.sendMessage(
+        chatId,
+        `Бот загад цифру ${chats[chatId]}`,
+        againOptions
+      );
     }
   });
 };
